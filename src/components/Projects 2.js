278cd4872f8a217191d8abc2321f07e01558b712
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2023-01-24 15:45:03 -0600

    adj devInfo for mobile devices

---
+++ src/components/Projects 2.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import "../styling/Projects.css";
+
+
+
+export class Projects extends Component{
+    render() {
+        return (
+
+            <div id="projects">
+                <div>tsumami</div>
+                <div>konami</div>
+                <div>weathermap</div>
+                <div>java contact manager</div>
+            </div>
+
+    )
+    }
+}
+
+
+
