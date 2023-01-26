b55b5487b10441f82efae97cf758d0c9d1a5af4b
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2023-01-25 23:44:42 -0600

    deleted reportWebVitals

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './styling/index.css';
 import App from './App';
 import './styling/Navbar.css';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
 
@@ -14,7 +13,4 @@ root.render(
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
